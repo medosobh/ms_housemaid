9f2b24c06a09a4f71993110c97b74ecd6f6324e5
medosobh <medo.sobh76@gmail.com>
2023-08-06 23:18:42 +0300

    update

--- __manifest__.py
+++ __manifest__.py
@@ -18,7 +18,6 @@
         'security/security.xml',
         'security/ir.model.access.csv',
         'data/sequence_views.xml',
-        # 'views/maid_search.xml',
         'views/menu.xml',
         'views/offices.xml',
         'views/maids.xml',
