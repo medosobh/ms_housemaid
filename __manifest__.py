db1c6e8def7528568d22c7f44d85809b81cb86ce
medosobh <medo.sobh76@gmail.com>
2023-08-15 22:03:42 +0300

    update

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Housemaid Sales and Operation System',
-    'version': '1.0',
+    'version': '15.0.1.0',
     'description': 'A system that help to maintain maids data and manage match \
          and hiring maid ',
     'summary': '',
