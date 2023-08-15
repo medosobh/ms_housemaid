03012b001e8eb98d72a360c3ef23a7feb215dc22
medosobh <medo.sobh76@gmail.com>
2023-08-16 02:15:44 +0300

    update

--- controllers/maidsdata.py
+++ controllers/maidsdata.py
@@ -9,10 +9,23 @@ from odoo.exceptions import UserError, ValidationError
 
 
 class maids(http.Controller):
-    @http.route('/housemaid/maids/', website=True, auth='user')
+    @http.route('/mymaids/', website=True, auth='user', type="http")
     def housmaid_maids(self, **kw):
-        # return "hello external office"
-        maids = request.env['ms_housemaid.maids'].sudo().search()
+        # return "hello external office"        
+        maids = request.env['housemaid.maids'].sudo().search([])
         return request.render("ms_housemaid.maids_page", {
             'maids': maids
         })
+        
+    @http.route('/maidform', website=True, auth='user', type="http")
+    def maid_form(self, **kw):
+        print("open form")
+        maids = request.env['housemaid.maids'].sudo().search([])
+        return request.render('ms_housemaid.maid_form',{
+        })
+    
+    @http.route('/create/webmaid', website=True, auth='user', type="http")
+    def maid_form(self, **kw):
+        print("open form")
+        request.env['housemaid.maids'].sudo().create(kw)
+        return request.render('ms_housemaid.create_success',{})
