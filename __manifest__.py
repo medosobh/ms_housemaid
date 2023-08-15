ea544cf3a35c3b1d56fdbe3adad98858b2bb1aae
medosobh <medo.sobh76@gmail.com>
2023-08-15 23:33:40 +0300

    update

--- __manifest__.py
+++ __manifest__.py
@@ -14,6 +14,7 @@
         'mail',
         'resource',
         'web',
+        'website',
         'web_kanban_gauge',
     ],
     'data': [
--- controllers/maidsdata.py
+++ controllers/maidsdata.py
@@ -3,7 +3,7 @@
 #    License, author and contributors information in:                         #
 #    __manifest__.py file at the root folder of this module.                  #
 ###############################################################################
-from odoo import http
+from odoo import http, _
 from odoo.http import request
 from odoo.exceptions import UserError, ValidationError
 
@@ -12,4 +12,4 @@ class maids(http.Controller):
     @http.route('/housemaid/maids/', website=True, auth='user')
     def housmaid_maids(self, **kw):
         # return "hello external office"
-        return request.render("ms_housmaid.maids_page", {})
+        return request.render("ms_housemaid.maids_page", {})
