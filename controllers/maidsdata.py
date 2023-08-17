26cd681bd2e7e6e9d2e8a3d879ac8fdb3ff202fc
medosobh <medo.sobh76@gmail.com>
2023-08-17 17:06:16 +0300

    update

--- controllers/maidsdata.py
+++ controllers/maidsdata.py
@@ -6,7 +6,7 @@
 from odoo import http, _
 from odoo.http import request
 from odoo.exceptions import UserError, ValidationError
-from odoo.addons.portal.controllers.portal import CustomerPortal, pager
+from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager
 
 
 class maidsportal(CustomerPortal):
@@ -16,24 +16,47 @@ class maidsportal(CustomerPortal):
         rtn['maids_count'] = request.env['housemaid.maids'].search_count([])
         return rtn
 
-    @http.route(['/my/maids', '/my/mades/page/<int:page>'], website=True, auth='user', type="http")
-    def my_maids_list_view(self, page=1, **kw):
+    @http.route(['/my/maids', '/my/maids/page/<int:page>'], type="http", website=True, auth='user')
+    def my_maids_list_view(self, page=1, sortby=None, **kw):
+        searchbar_sortings = {
+            'id': {'label': _('ID Desc'), 'order': 'id desc'},
+            'name': {'label': _('Name'), 'order': 'name'},
+            'country_id': {'label': _('Country'), 'order': 'country_id'},
+        }
+        # default sort by order
+        if not sortby:
+            sortby = 'id'
+        order = searchbar_sortings[sortby]['order']
 
         total_maids = request.env['housemaid.maids'].sudo().search_count([])
-        page_details = pager(
+
+        pager = portal_pager(
             url='/my/maids',
+            url_args={'sortby': sortby},
             total=total_maids,
             page=page,
-            step=5
+            step=10,
+
         )
         maids = request.env['housemaid.maids'].sudo().search(
-            [], limit=5, offset=page_details['offset'])
+            [],
+            limit=10,
+            order=order,
+            offset=pager['offset'],
+        )
+
         vals = {
             'maids': maids,
             'page_name': 'my_maids_portal_list_view',
-            'pager': page_details,
+            'pager': pager,
+            'searchbar_sortings': searchbar_sortings,
+            'sortby': sortby,
         }
-        return request.render("ms_housemaid.my_maids_portal_list_view", vals)
+
+        return request.render(
+            "ms_housemaid.my_maids_portal_list_view",
+            vals,
+        )
 
     @http.route(['/my/maids/<model("housemaid.maids"):maids_id>'], website=True, auth='user', type="http")
     def my_maids_form_view(self, maids_id, **kw):
@@ -49,7 +72,7 @@ class maidsportal(CustomerPortal):
         if maids_index != 0 and maids_ids[maids_index - 1]:
             vals['prev_record'] = format(
                 maids_ids[maids_index-1])
-        if maids_index < len(maids_ids) -1 and maids_ids[maids_index + 1]:
+        if maids_index < len(maids_ids) - 1 and maids_ids[maids_index + 1]:
             vals['next_record'] = format(
                 maids_ids[maids_index+1])
 
