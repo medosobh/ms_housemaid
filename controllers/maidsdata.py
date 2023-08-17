23b471f66afa1ceeb236f97888b243f2d7ca23a7
medosobh <medo.sobh76@gmail.com>
2023-08-17 21:07:10 +0300

    update

--- controllers/maidsdata.py
+++ controllers/maidsdata.py
@@ -17,26 +17,32 @@ class maidsportal(CustomerPortal):
         return rtn
 
     @http.route(['/my/maids', '/my/maids/page/<int:page>'], type="http", website=True, auth='user')
-    def my_maids_list_view(self, page=1, sortby=None, **kw):
+    def my_maids_list_view(self, page=1, sortby=None, groupby=None, **kw):
         searchbar_sortings = {
-            'id': {'label': _('ID Desc'), 'order': 'id desc'},
-            'name': {'label': _('Name'), 'order': 'name'},
-            'country_id': {'label': _('Country'), 'order': 'country_id'},
+            'id': {'label': 'ID Desc', 'order': 'id desc'},
+            'name': {'label': 'Name', 'order': 'name'},
+            'country_id': {'label': 'Country', 'order': 'country_id'},
+        }
+        searchbar_groupby = {
+            'country_id': {'input': 'country_id', 'label': 'Country', 'order': 1},
+            'jobs_id': {'input': 'jobs_id', 'label': 'Jobs', 'order': 1},
         }
         # default sort by order
         if not sortby:
             sortby = 'id'
         order = searchbar_sortings[sortby]['order']
+        # default groupby
+        if not groupby:
+            groupby = 'None'
 
         total_maids = request.env['housemaid.maids'].sudo().search_count([])
-
+        maid_url = '/my/maids'
         pager = portal_pager(
-            url='/my/maids',
-            url_args={'sortby': sortby},
+            url=maid_url,
+            url_args={'sortby': sortby, 'groupby': groupby},
             total=total_maids,
             page=page,
             step=10,
-
         )
         maids = request.env['housemaid.maids'].sudo().search(
             [],
@@ -44,13 +50,15 @@ class maidsportal(CustomerPortal):
             order=order,
             offset=pager['offset'],
         )
-
         vals = {
+            'default_url': maid_url,
             'maids': maids,
             'page_name': 'my_maids_portal_list_view',
             'pager': pager,
             'searchbar_sortings': searchbar_sortings,
+            'searchbar_groupby': searchbar_groupby,
             'sortby': sortby,
+            'groupby': groupby,
         }
 
         return request.render(
@@ -78,7 +86,8 @@ class maidsportal(CustomerPortal):
 
     @http.route(['/my/maids/print/<model("housemaid.maids"):maids_id>'], website=True, auth='user', type="http")
     def my_maids_report_print(self, maids_id, **kw):
-        return
+
+        return self._show_report(self, model=maids_id, report_type='pdf', report_ref='', download=True)
 
 
 class createmaids(http.Controller):
